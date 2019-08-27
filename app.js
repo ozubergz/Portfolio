9a6d04939e15e716c3bc9fdba86798ce7a8d92cc
ozubergs <34900230+ozubergs@users.noreply.github.com>
2019-08-26 23:56:16 -0400

    add contact

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post("/contact", function(req, res) {
+  let email = req.body.email,
+      subject = req.body.subject,
+      message = req.body.message;
+
+  console.log(email, subject, message)
+  
+  
 });
 
 app.listen(3000, function() {
